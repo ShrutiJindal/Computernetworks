be39761918592a4da1baf383fc5914fac0e65b5e
ManishaDewal <manishadewal93@gmail.com>
2017-10-27 18:46:01 -0400

    Added the Message Type file

--- MessageType.java
+++ MessageType.java
@@ -1,14 +1,36 @@
 import java.io.IOException;
 
-public class MessageType implements Runnable {
-	
-	public MessageType(ClientConnect aClient, TCPConnect myConnection) 
-	{
-		
+public class MessageType {
+
+	// protected static final long serialVersionUID = 1L;
+	String msgType;
+	int msgLength;
+	byte[] messagePayload;
+
+	// setter functions
+	public void setMsgType(String msgType) {
+		this.msgType = msgType;
 	}
-	
-	@Override
-	public void run()
-	{
+
+	public void setMsgLength(int msgLength) {
+		this.msgLength = msgLength;
 	}
+
+	public void setMessagePayload(byte[] messagePayload) {
+		this.messagePayload = messagePayload;
+	}
+
+	// getter functions
+	public String getMsgType() {
+		return msgType;
+	}
+
+	public int getMsgLength() {
+		return msgLength;
+	}
+
+	public byte[] getMessagePayload() {
+		return messagePayload;
+	}
+
 }
